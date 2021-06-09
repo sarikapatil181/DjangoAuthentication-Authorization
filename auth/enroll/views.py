e60ce15e0e8bbbce891017ac1bc8a75b1564e2cb
Your Name <sarikapatil181@gmail.com>
2021-06-09 17:53:14 +0400

    commit 3

--- auth/enroll/views.py
+++ auth/enroll/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render
+from django.contrib import messages
 from .forms import SignUpForm
 
 # Create your views here.
@@ -6,6 +7,7 @@ def sign_up(request):
     if request.method=='POST':
         fm=SignUpForm(request.POST)
         if fm.is_valid():
+            messages.success(request,'Account Created Successfully!!')
             fm.save()
     else:
         fm=SignUpForm()
